b49861a6ec0da3e579c3cd93e6ef984bfe2051f5
Mark de Jong <mdejong83@gmail.com>
2013-11-08 17:25:54 +0100

    Added Fleistr cover images plugin initiative

--- assets/js/index.js
+++ assets/js/index.js
@@ -17,7 +17,7 @@ ghost.themes.zeitgeist = ghost.themes.zeitgeist || {
      * Load Instafeed resources
      * http://instafeedjs.com
      **/
-    ghostFeed: function ghostFeed() {
+    ghostFeed: function () {
         // "use strict";
         var ghostFeed;
         ghostFeed = new Instafeed({
@@ -72,10 +72,118 @@ ghost.themes.zeitgeist = ghost.themes.zeitgeist || {
           }
         });
         ghostFeed.run();
+    },
+
+    //Grabbing Flickr Pics
+    flickrFeed: function(targetEl) {
+      "use strict";
+
+      //-- Flickr url/REST locations
+      var FLICKR = {
+          REST_URL: '//www.flickr.com/services/rest/',
+          SEARCH: 'flickr.photos.search',
+          GETINFO: 'flickr.photos.getInfo',
+          PLACEINFO: 'flickr.places.getInfo'
+      };
+      //-- Flickr parameters
+      var flickr = {
+            'api_key': 'ce2cebcf703d8ab2bdbd5342f16ebcaa',
+            'method': FLICKR.SEARCH, 'format': 'json', 'tags': 'movember', 'safe_search': '1',
+            'per_page': '5', 'jsoncallback': '?', 'sort': 'interestingness-desc', 'extras': 'url_z,geo', 'has_geo': '1'
+          };
+
+      //-- Default settings
+      var photos = [],
+          $targetDOM = $(targetEl),
+          settings = {
+              mock: false,
+              random: Math.floor(Math.random() * flickr.per_page)
+          };
+
+      //-- Flickr request URL
+      var flickrSearchUrl = FLICKR.REST_URL +
+              '?method=' + flickr.method + '&api_key='+ flickr.api_key + '&tags=' + flickr.tags +
+              '&format=' + flickr.format + '&per_page=' + flickr.per_page + '&safe_search=' + flickr.safe_search + '&nojsoncallback=' + '1' +
+              '&sort=' + flickr.sort + '&has_geo=' + flickr.has_geo + '&extras=' + flickr.extras,
+          flickrSearchData;
+
+      if(settings.mock === true) flickrSearchUrl = '/assets/js/flickr.photos.search.js';
+
+      var fetchFlickrPic = function() {
+        $.ajax({
+          url: flickrSearchUrl,
+          dataType: 'json',
+          data: JSON,
+          error: function(data) {
+            console.info(data.statusText);
+          },
+          success: function(data) {
+            console.info(data.stat);
+            $.each(data.photos.photo, function(i, item){
+              photos.push({
+                'id' : item.id,
+                'details' : {
+                  'src': item.url_z,
+                  'title': item.title,
+                  'place_id': item.place_id,
+                  'geo': { 'city': '', 'country': '' }
+                }
+              });
+            });
+          },
+          complete: function(data) {
+            console.info(data.statusText);
+            $targetDOM.addClass('cover-loading').html('<div class="cover-image" style="background-image: url('+photos[settings.random].details.src+')">' +
+              '<div class="cover-details clearfix"><p>'+ photos[settings.random].details.title +'</p></div></div>');
+            fetchFlickrGeo(photos[settings.random].id, photos[settings.random].details.place_id);
+          }
+        });
+      }
+
+      var fetchFlickrGeo = function(photo_id, place_id) {
+        var flickrGetinfoUrl = FLICKR.REST_URL +
+            '?method=' + FLICKR.PLACEINFO +
+            '&api_key=' + flickr.api_key +
+            '&place_id=' + place_id +
+            '&format=' + flickr.format +
+            '&nojsoncallback=' + '1';
+        if(settings.mock === true) flickrGetinfoUrl = '/assets/js/flickr.place.getinfo.js';
+
+        $.ajax({
+          url: flickrGetinfoUrl,
+          dataType: 'json',
+          data: JSON,
+          error: function(data) {
+            console.info('Place: fail!');
+            console.info(data);
+          },
+          success: function(data) {
+            var city = data.place.woe_name,
+                country = data.place.country._content;
+
+            $.each(photos, function(i, object) {
+              if(object.id === photo_id) {
+                photos[i].details.geo = {
+                  'city': city, 'country': country
+                };
+                $('.cover-details p', $targetDOM).append('<br/><i class="fi-marker"></i>' +
+                  photos[i].details.geo.city+', '+photos[i].details.geo.country);
+              }
+            });
+          },
+          complete: function(data){
+            // 'place: complete'
+            $targetDOM.removeClass('cover-loading');
+          }
+        });
+      }
+
+      return fetchFlickrPic();
     }
+
 }
 $(document).ready(function() {
 
-    ghost.themes.zeitgeist.ghostFeed();
+    ghost.themes.zeitgeist.flickrFeed('#cover-image');
 
 });
